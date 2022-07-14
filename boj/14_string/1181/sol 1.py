e9055b059348bd0ce419d6804c7400f7f2588d59
Ryu4949 <kyong494949@gmail.com>
2022-07-14 23:34:20 +0900

    220714

---
+++ boj/14_string/1181/sol 1.py
@@ -0,0 +1,12 @@
+N = int(input())
+words = []
+
+for _ in range(N):
+    word = input()
+    if word not in words:
+        words.append(word)
+
+words.sort(key=lambda x: (len(x), x))
+
+for i in words:
+    print(i)
