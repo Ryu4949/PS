1b7c4afc817d26a835a194510d9f4535ad69f60c
Ryu4949 <kyong494949@gmail.com>
2022-09-06 23:30:59 +0900

    220906

--- boj/13_bitmasking/2087/sol 1.py
+++ boj/13_bitmasking/2087/sol 1.py
@@ -22,5 +22,18 @@ for i in range(1<<len(right)):
             total += right[j]
     right_sum.append((total, i<<len(left)))
 
-print(left_sum)
-print(right_sum)
+left_sum.sort()
+right_sum.sort()
+
+for i in range(len(left_sum)):
+    s = 0
+    e = len(right_sum)-1
+    while s <= e:
+        mid = (s+e)//2
+        if left_sum[i][0] + right_sum[mid][0] == K:
+            print(bin(left_sum[i][1]+right_sum[mid][1])[2:].zfill(N)[::-1])
+            exit()
+        elif left_sum[i][0] + right_sum[mid][0] > K:
+            e = mid-1
+        else:
+            s = mid+1
