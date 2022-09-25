0d99d3d298ac009f99418a019b55b66a12969898
Ryu4949 <kyong494949@gmail.com>
2022-09-25 23:56:13 +0900

    220925

---
+++ boj/00_etc/1284/sol 1.py
@@ -0,0 +1,14 @@
+while True:
+    num = input()
+    if num == '0':
+        break
+    total = 0
+    for n in num:
+        if n == '1':
+            total += 2
+        elif n == '0':
+            total += 4
+        else:
+            total += 3
+    total += 1+len(num)
+    print(total)
