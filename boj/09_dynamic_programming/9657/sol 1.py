7848c08e40cf4bdd1285d1bc2221d838107e2f9b
Ryu4949 <kyong494949@gmail.com>
2022-10-08 23:11:42 +0900

    221008

---
+++ boj/09_dynamic_programming/9657/sol 1.py
@@ -0,0 +1,15 @@
+N = int(input())
+dp = [0] * 1001
+
+dp[1] = 'SK'
+dp[3] = 'SK'
+dp[4] = 'SK'
+
+for i in range(2, N+1):
+    if dp[i] == 0:
+        if dp[i-1] == 'CY' or dp[i-3] == 'CY' or dp[i-4] == 'CY':
+            dp[i] = 'SK'
+        else:
+            dp[i] = 'CY'
+
+print(dp[N])
